5ee6a9abcb283db950f6cc4f4a2e983ff51f9f8e
Oscar Rosete <oscaralonso11@hotmail.com>
2020-06-15 03:49:25 -0700

    primer upload funcional

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,6 @@
 export var GLOBAL = {
-    url: 'http://localhost:4000',
+    // url: 'http://localhost:4000',
+    url: 'https://freelancer-angular-project.herokuapp.com',
     backendUrl: 'https://freelancer-angular-project.herokuapp.com',
     header_color: '#1687A6'
 };
